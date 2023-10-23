0434459b287fcdbe78292c2badecdd6abe96d8e1
jscastanos <jec.castanos@gmail.com>
2023-10-23 23:05:24 +0800

    fix: responsive and remove period

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const { theme } = context;
 
   return (
-    <div className='space-x-4 flex-1 flex justify-center items-center'>
+    <div className='flex justify-center items-center flex-1 flex-col sm:flex-row space-y-4 sm:space-x-4'>
       <div>
         {theme === 'dark' && (
           <Image
@@ -35,7 +35,7 @@ export default function Home() {
       <div className='space-y-2'>
         <p className='text-base sm:text-xl md:text-2xl xl:text-3xl'>Hello,</p>
         <h1 className='text-4xl sm:text-6xl md:text-7xl xl:text-8xl'>
-          I’m <span className='font-semibold '>Jec Castanos</span>.
+          I’m <span className='font-semibold '>Jec Castanos</span>
         </h1>
         <p className='text-lg sm:text-2xl md:text-3xl xl:text-4xl'>
           Frontend Developer
