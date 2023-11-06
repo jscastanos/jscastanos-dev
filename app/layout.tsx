00c542c8ed8861dfaa93bd6164cf7d9304f8902b
jscastanos <jec.castanos@gmail.com>
2023-11-06 17:06:38 +0800

    seo: improve description and add icons and openGraph images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,37 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import AppLayout from './components/AppLayout';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import AppLayout from "./components/AppLayout";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Jec Castanos',
+  title: "Jec Castanos - Frontend Engineer",
   description:
-    'Hi, I’m Jec Castanos. I’m a frontend engineer from The Philippines',
+    "Hi, I’m Jec Castanos. I am a frontend engineer from the Philippines with over 5 years of experience in web and software development. I am passionate about building user-friendly and visually appealing web applications. I am also an expert in performance optimization and code quality. In my spare time, I enjoy learning new technologies, playing video games, and spending time with my family and friends. I am always looking for new opportunities to challenge myself and grow as an engineer. If you are looking for a frontend engineer with a proven track record of success, I would love to hear from you.",
+  authors: [
+    {
+      url: "https://github.com/jscastanos",
+      name: "Jec Castanos",
+    },
+  ],
+  icons: [
+    {
+      url: "https://www.jscastanos.dev/day-owl.svg",
+    },
+  ],
+  openGraph: {
+    images: [
+      {
+        url: "https://www.jscastanos.dev/day-owl.svg",
+        alt: "Jec Castanos - Day Owl",
+      },
+      {
+        url: "https://www.jscastanos.dev/night-owl.svg",
+        alt: "Jec Castanos - Night Owl",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <AppLayout>{children}</AppLayout>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-'use client';
-import Image from 'next/image';
-import { useContext } from 'react';
-import { ThemeContext } from './components/AppLayout';
+"use client";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "./components/AppLayout";
 
 export default function Home() {
   const context = useContext(ThemeContext);
@@ -11,33 +11,33 @@ export default function Home() {
   const { theme } = context;
 
   return (
-    <div className='flex justify-center items-center flex-1 flex-col sm:flex-row space-y-4 sm:space-x-4'>
+    <div className="flex justify-center items-center flex-1 flex-col sm:flex-row space-y-4 sm:space-x-4">
       <div>
-        {theme === 'dark' && (
+        {theme === "dark" && (
           <Image
-            className='rounded-full'
-            src='/night-owl.svg'
-            alt='owl'
+            className="rounded-full"
+            src="/night-owl.svg"
+            alt="owl"
             width={200}
             height={200}
           />
         )}
-        {theme === 'light' && (
+        {theme === "light" && (
           <Image
-            className='rounded-full'
-            src='/day-owl.svg'
-            alt='owl'
+            className="rounded-full"
+            src="/day-owl.svg"
+            alt="owl"
             width={200}
             height={200}
           />
         )}
       </div>
-      <div className='space-y-2'>
-        <p className='text-base sm:text-xl md:text-2xl xl:text-3xl'>Hello,</p>
-        <h1 className='text-4xl sm:text-6xl md:text-7xl xl:text-8xl'>
-          I’m <span className='font-semibold '>Jec Castanos</span>
+      <div className="space-y-2">
+        <p className="text-base sm:text-xl md:text-2xl xl:text-3xl">Hello,</p>
+        <h1 className="text-4xl sm:text-6xl md:text-7xl xl:text-8xl">
+          I’m <span className="font-semibold ">Jec Castanos</span>
         </h1>
-        <p className='text-lg sm:text-2xl md:text-3xl xl:text-4xl'>
+        <p className="text-lg sm:text-2xl md:text-3xl xl:text-4xl">
           Frontend Developer
         </p>
       </div>
