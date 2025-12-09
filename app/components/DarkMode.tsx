1b6aa7e681385af0917d0431edab66a8e5c7a21b
jscastanos <jec.castanos@gmail.com>
2025-12-09 16:24:26 +0800

    chore: upgrade next and use biome

--- app/components/DarkMode.tsx
+++ app/components/DarkMode.tsx
@@ -1,26 +1,30 @@
-'use client';
+"use client";
 
-import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
-  theme: 'light' | 'dark';
-  setTheme: (theme: 'light' | 'dark') => void;
+	theme: "light" | "dark";
+	setTheme: (theme: "light" | "dark") => void;
 }
 
 export default function DarkMode({ theme, setTheme }: Props) {
-  function handleMode() {
-    const nextTheme = theme === 'dark' ? 'light' : 'dark';
+	function handleMode() {
+		const nextTheme = theme === "dark" ? "light" : "dark";
 
-    // set mode to local storage
-    localStorage.setItem('theme', nextTheme);
-    setTheme(nextTheme);
-  }
+		// set mode to local storage
+		localStorage.setItem("theme", nextTheme);
+		setTheme(nextTheme);
+	}
 
-  return (
-    <button className='text-2xl h-8 w-8 outline-0' onClick={handleMode}>
-      {theme === 'dark' && <FontAwesomeIcon icon={faMoon} />}
-      {theme === 'light' && <FontAwesomeIcon icon={faSun} />}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			className="text-2xl h-8 w-8 outline-0"
+			onClick={handleMode}
+		>
+			{theme === "dark" && <FontAwesomeIcon icon={faMoon} />}
+			{theme === "light" && <FontAwesomeIcon icon={faSun} />}
+		</button>
+	);
 }
